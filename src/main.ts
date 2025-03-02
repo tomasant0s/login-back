229a6fd4ec1500ec258cd305aed3497efd6d50e6
Tomás Santos <132064867+tomasant0s@users.noreply.github.com>
2025-03-02 17:59:08 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 
 async function bootstrap() {
-  // Ajuste os caminhos conforme sua estrutura. Aqui assumimos que o código compilado está em dist/
-  // e os certificados estão um nível acima (na raiz do projeto).
   const sslOptions = {
     key: fs.readFileSync(path.join(__dirname, '../private.key')),
     cert: fs.readFileSync(path.join(__dirname, '../certificate.crt')),
@@ -15,7 +13,6 @@ async function bootstrap() {
       : undefined,
   };
 
-  // Cria a aplicação com as opções de HTTPS
   const app = await NestFactory.create(AppModule, { httpsOptions: sslOptions });
 
   app.useGlobalPipes(
@@ -31,12 +28,12 @@ async function bootstrap() {
     ],
   });
 
-  // Definindo a porta a partir da variável de ambiente ou utilizando 8080 como padrão
   const port = process.env.PORT || 8080;
   await app.listen(port);
-  
-  // Exibindo no console a porta onde o servidor está rodando
-  console.log(`Servidor rodando na porta ${port}`);
+
+  // Utilize o Logger do Nest para registrar a mensagem
+  const logger = new Logger('Bootstrap');
+  logger.log(`Servidor rodando na porta ${port}`);
 }
 
 bootstrap();
